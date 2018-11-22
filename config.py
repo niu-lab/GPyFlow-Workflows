d43ae7219260d4293d18708c651efa731e8041b3
ZhangYu <zhangyu.star@bytedance.com>
2018-11-22 15:21:18 +0800

    change to mysql

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 WORKFLOWS_DIR = os.path.join(BASE_DIR, "workflows")
-dbpath = os.path.join(BASE_DIR, "data.sqlite3")
+# dbpath = os.path.join(BASE_DIR, "data.sqlite3")
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + dbpath
 WHOOSHEE_MIN_STRING_LEN = 1
 SECRET_KEY = 'secret_key'
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://workflow:{passwd}@localhost:{port}/workflows".format(
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://workflow:{passwd}@localhost:{port}/workflow".format(
     passwd=os.environ.get("WORKFLOW_PASSWD"), port=os.environ.get("MYSQL_PORT"))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_COMMIT_TEARDOWN = True
